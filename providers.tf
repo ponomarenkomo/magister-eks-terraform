d90a7878294737723870eb38887e4cb063fa937c
nikita.ponomarenko <ponomarenko.n.2001@gmail.com>
2024-01-22 12:39:02 +0200

    pipeline

--- providers.tf
+++ providers.tf
@@ -17,26 +17,26 @@ module "backend" {
     state_bucket = var.state_bucket
 }
 
-module "vpc" {
-  source = "./vpc"
-  cluster_name = var.cluster_name
-  vpc_name = var.vpc_name
-  vpc_cidr = var.vpc_cidr
-  public_subnets = var.public_subnets
-  private_subnets = var.private_subnets
-  azs = var.azs
-  enable_nat_gateway = var.enable_nat_gateway
-  enable_vpn_gateway = var.enable_vpn_gateway
-  one_nat_gateway_per_az = var.one_nat_gateway_per_az
-  enable_dns_hostnames = var.enable_dns_hostnames
-  enable_dns_support = var.enable_dns_support
+# module "vpc" {
+#   source = "./vpc"
+#   cluster_name = var.cluster_name
+#   vpc_name = var.vpc_name
+#   vpc_cidr = var.vpc_cidr
+#   public_subnets = var.public_subnets
+#   private_subnets = var.private_subnets
+#   azs = var.azs
+#   enable_nat_gateway = var.enable_nat_gateway
+#   enable_vpn_gateway = var.enable_vpn_gateway
+#   one_nat_gateway_per_az = var.one_nat_gateway_per_az
+#   enable_dns_hostnames = var.enable_dns_hostnames
+#   enable_dns_support = var.enable_dns_support
 
-}
+# }
 
-module "eks" {
-  source = "./eks"
-  cluster_name = var.cluster_name
-  vpc_id = module.vpc.vpc_id
-  vpc_private_subnets = module.vpc.private_subnets
+# module "eks" {
+#   source = "./eks"
+#   cluster_name = var.cluster_name
+#   vpc_id = module.vpc.vpc_id
+#   vpc_private_subnets = module.vpc.private_subnets
 
-}
+# }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "aws_region" {
   type = string
+  default = "us-east-1"
 }
 
 variable "state_bucket" {
   type    = string
+  default = "interesting-bucket-for-test-yes"
 
 }
 
